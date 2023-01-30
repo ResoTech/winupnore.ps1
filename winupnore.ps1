a020147d9665515d055eb70d67b3359a2981044f
Christopher Kelley <christopher@cktech.org>
2023-01-30 15:34:35 -0500

    Update winupnore.ps1
    Replaced "install-WindowsUpdate" with "Get-WindowsUpdate

--- winupnore.ps1
+++ winupnore.ps1
@@ -18,7 +18,7 @@ $Updates = Get-WUInstall
 # Install updates
 if ($Updates) {
     Write-Output "Installing updates..."
-    Install-WindowsUpdate -Install -AcceptAll -IgnoreReboot -ScheduleReboot 01.31.2023 00:00:00
+    Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot -ScheduleReboot 01.31.2023 00:00:00
 }
 else {
     Write-Output "No updates available."
